c54f0775fdfadb8426fdcc75b500be32f4ee2ca0
Akash Yadav <akashyadav61234@gmail.com>
2023-11-08 09:57:32 +0530

    hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,14 @@
 import Navbar from "./Component/Navbar/Navbar";
+import HeroImage from "./Component/HeroImage/HeroImage";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="heroimage">
+        <HeroImage />
+      </div>
     </div>
   );
 }
---
+++ qtify/src/Component/HeroImage/HeroImage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./HeroImage.css";
+import Logo1 from "../../Images/Logo1";
+
+function HeroImage() {
+  return (
+    <div className="heroalign">
+      <div className="text-color">
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <div className="logo-layout">
+        <Logo1 />
+      </div>
+    </div>
+  );
+}
+
+export default HeroImage;
